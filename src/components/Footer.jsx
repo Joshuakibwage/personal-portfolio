51298dd3fe13a7332fd9e61ae869c6026cd30058
Josh <joshuakibwage23@gmail.com>
2025-05-08 20:47:09 +0300

    Add footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="w-full py-24">
+      <p className="text-center">
+        &copy; {new Date().getFullYear()} Joshua Kibwage. All rights reserved.
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,7 @@ import AboutMe from '../components/AboutMe';
 import Skills from '../components/Skills';
 import Projects from '../components/Projects';
 import Contact from '../components/Contact';
+import Footer from '../components/Footer';
 
 
 
@@ -27,7 +28,8 @@ const Home = () => {
         <Projects />
         <Contact />
       </main>
-
+      {/* footer */}
+      <Footer />
     </div>
   )
 }
